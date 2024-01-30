d2959248d7f362cccedf138b4ee206ca250002d7
Apk <111860693+MrApk@users.noreply.github.com>
2024-01-30 23:31:08 +0530

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,5 +1,14 @@
-var http = require('http');
-http.createServer(function(req, res) {
-  res.write("Hi APK");
-  res.end();
-}).listen(8080);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static(__dirname + ''));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
