04136286e3181545f72944832ea1dba2d1bc23fe
Apk <111860693+MrApk@users.noreply.github.com>
2024-01-31 18:20:55 +0530

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,5 @@
-// app.js
-const http = require('http');
-http.createServer(function (req, res) {
-  res.write("hi APK bhai");
+var http = require('http');
+http.createServer(function(req, res) {
+  res.write("I'm alive");
   res.end();
 }).listen(8080);
